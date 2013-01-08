e70eac1956350f036c8da7e58c331802deaf2675
webdorado <webdorado@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-08 12:43:13 +0000

    fix swf bug
    git-svn-id: https://plugins.svn.wordpress.org/player/trunk@649708 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Player.php
+++ Player.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Spider Video Player
 Plugin URI: http://web-dorado.com/
-Version: 1.3.3
+Version: 1.3.4
 Author: http://web-dorado.com/
 License: GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 */
