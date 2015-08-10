a58457e1d6798fc376399f0e161da4d19a2e6bbf
webdorado <webdorado@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-10 15:53:53 +0000

    New release
    git-svn-id: https://plugins.svn.wordpress.org/player/trunk@1217401 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Player.php
+++ Player.php
@@ -3,7 +3,7 @@
 Plugin Name: Spider Video Player 
 Plugin URI: https://web-dorado.com/products/wordpress-player.html
 Description:Spider Video Player supports both HTML5 and Flash, allowing you to play videos on any mobile device.Spider WordPress Video Player allows you to easily add videos to your website with the possibility of organizing videos into playlists and choosing a preferred layout for the player.
-Version: 1.5.13
+Version: 1.5.14
 Author: https://web-dorado.com/
 License: GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 */
@@ -2483,7 +2483,7 @@ function Spider_Video_Player_front_end($id)
                 pause_<?php echo $ident ?>.css('display', '');
             }
             if (jQuery('#repeat_<?php echo $ident ?>').val() == 'repeatAll') {
-                jQuery('#global_body_<?php echo $ident ?> .functiom<?php echo $ident ?>').click();
+                jQuery('#global_body_<?php echo $ident ?> .playNext_<?php echo $ident ?>').click();
                
             }
         }
